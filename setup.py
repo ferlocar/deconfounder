3d32bf87c1a8cfc9a5b0ce69610e1026935977d0
yanfangh <houyanfang7@gmail.com>
2023-05-11 10:15:52 +0800

    add new deconfounder

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from Cython.Build import cythonize
 import numpy
 
 setup(ext_modules=cythonize("deconfounder/mse_causal.pyx"), include_dirs=[numpy.get_include()])
-setup(ext_modules=cythonize("deconfounder/mse_deconfound.pyx"), include_dirs=[numpy.get_include()])
+setup(ext_modules=cythonize("deconfounder/mse_deconfound_legacy.pyx"), include_dirs=[numpy.get_include()])
+setup(ext_modules=cythonize("deconfounder/deconfound_criterion.pyx"), include_dirs=[numpy.get_include()])
 
 # To build the Cython files, run the commands below:
 # python setup.py build_ext --inplace

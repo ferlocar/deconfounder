699a7c1bff75688d3b9d6a354414082defa80cc4
yanfangh <houyanfang7@gmail.com>
2023-06-07 15:03:13 +0800

    remove deconfounder_swift

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ import numpy
 setup(ext_modules=cythonize("deconfounder/mse_causal.pyx"), include_dirs=[numpy.get_include()])
 setup(ext_modules=cythonize("deconfounder/mse_deconfound_legacy.pyx"), include_dirs=[numpy.get_include()])
 setup(ext_modules=cythonize("deconfounder/deconfound_criterion.pyx"), include_dirs=[numpy.get_include()])
-setup(ext_modules=cythonize("deconfounder/deconfound_criterion_swift.pyx"), include_dirs=[numpy.get_include()])
 
 # To build the Cython files, run the commands below:
 # python setup.py build_ext --inplace
